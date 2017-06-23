b373f50a0344fa184b974fb067301579c9201375
Kaspar Vollenweider <vok@panter.ch>
2017-06-23 10:53:29 +0200

    run.sh can now dump its methods to aliases in selected ZSH or Bash config

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ if [ "$1" == "status" ]; then
   exit 0
 fi
 
-if [ "$1" == "status" ]; then
+if [ "$1" == "kill" ]; then
   docker-compose kill middleman
   exit 0
 fi
@@ -60,7 +60,41 @@ if [ "$1" == "yarn" ]; then
   exit 0
 fi
 
+dumpAlias ()
+{
+  if (grep "$(echo $1 | cut -d '=' -f 1)" "$2" > /dev/null); then
+    echo "«$1» is allready in the file, so it is skipped."
+  else
+    echo "$1" >> "$2"
+  fi
+}
+
+
+if [ "$1" == "aliases" ]; then
+  if [ -z "$2" ]; then
+    echo "need to provide shell config to dump aliases into!"
+    exit 1
+  fi
+  if ! [ -f "$2" ]; then
+    echo "target config file doesn't exist"
+    exit 1
+  fi
+  dumpAlias "alias middle-start='sh -c \"docker-compose start || docker-compose up\"'" "$2"
+  dumpAlias "alias middle-stop='docker-compose stop'" "$2"
+  dumpAlias "alias middle-pause='docker-compose pause'" "$2"
+  dumpAlias "alias middle-resume='docker-compose unpause'" "$2"
+  dumpAlias "alias middle-status='docker-compose ps'" "$2"
+  dumpAlias "alias middle-build='docker-compose exec middleman bundle exec middleman build'" "$2"
+  dumpAlias "alias middle-bash='docker-compose exec middleman bash'" "$2"
+  dumpAlias "alias middle-logs='docker-compose logs middleman'" "$2"
+  dumpAlias "alias middle-gulp='docker-compose exec middleman node_modules/.bin/gulp'" "$2"
+  dumpAlias "alias middle-bundle='docker-compose exec middleman bundle'" "$2"
+  dumpAlias "alias middle-yarn='docker-compose exec middleman yarn'" "$2"
+  exit 0
+fi
+
 echo "Usage:"
+echo "./run.sh aliases ~/path/configfile - dump a bunch of alliases to you're BASH or ZSH config file"
 echo "./run.sh start - start or initially start the container"
 echo "./run.sh stop - stop the running container"
 echo "./run.sh pause - pause the container"
